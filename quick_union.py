2dbceb38941927fe334ac3c08082dd36da3ff74e
tfarrell-vz <farrell@abiresearch.com>
2015-01-25 20:45:38 -0500

    Quick Union implemented.

--- quick_union.py
+++ quick_union.py
@@ -18,3 +18,19 @@ class UF:
             else:
                 i = self.id[i]
         return root
+
+    def union(self, p, q):
+        """ Set the root of p (position in the array) to the root of q """
+        p_root = self.find_root(p)
+        q_root = self.find_root(q)
+        self.id[p_root] = q_root
+
+
+test = UF(5)
+print(test.id)
+test.union(1,3)
+test.union(1, 2)
+print(test.connected(1,3))
+print(test.connected(1,2))
+print(test.connected(1,4))
+print(test.id)
